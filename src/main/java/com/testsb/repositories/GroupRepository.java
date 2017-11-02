921319e6432fa8265e34a46d537fafb539b0d98d
RuiFoxx <fox.uchiha95@gmail.com>
2017-11-02 16:52:09 +0700

    [+] Spring application for testing database
    First name and last name from start page 'Welcome' message are extracted from local database.

---
+++ src/main/java/com/testsb/repositories/GroupRepository.java
@@ -0,0 +1,8 @@
+package com.testsb.repositories;
+
+import com.testsb.models.Groupgroup;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GroupRepository extends CrudRepository<Groupgroup, Integer> {
+
+}
---
+++ src/main/java/com/testsb/services/GroupService.java
@@ -0,0 +1,7 @@
+package com.testsb.services;
+
+import com.testsb.models.Groupgroup;
+
+public interface GroupService {
+    public Groupgroup getById(Integer id);
+}
