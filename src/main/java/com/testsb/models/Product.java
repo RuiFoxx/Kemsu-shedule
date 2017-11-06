8d1dfebdab58912947ee224a94d71352bfe7afc2
RuiFoxx <fox.uchiha95@gmail.com>
2017-11-06 18:41:20 +0700

    [+] Added models for Product and Product in Cart
    Refactoring from Students to Products

---
+++ src/main/java/com/testsb/models/Product.java
@@ -0,0 +1,46 @@
+package com.testsb.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+    private String name;
+    private int quantity;
+    private float price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
